e5957c5640d761e1d030fee0201987b9b0209c6d
Fabrizio-2025 <fabriziobc2020@outlook.com>
2023-05-11 04:49:48 -0500

    Feat: Adding Loggin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,ViewEncapsulation } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'practica';
+  userName:string = "";
+  password:string = "";
+  show = true;
+
+  toggleButton(){
+    if(this.show){
+      this.show = false;
+    }
+    else{
+      this.show = true
+    }
+  }
 }
